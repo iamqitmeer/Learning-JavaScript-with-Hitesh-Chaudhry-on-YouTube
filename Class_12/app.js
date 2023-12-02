6c6e0ceb40fc372c6be9ce31359e4d09f56d30c7
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2023-12-02 11:55:41 +0500

    Array in Javascript

---
+++ Class_12/app.js
@@ -0,0 +1,57 @@
+// Array in Javascript
+
+// Array Hamesha Square Bracket ke andar likhte he. or square bracket kke andar jo hota he usko ham kehte he elementts. ab elementts string bhi hoskte he Number bhi hoskte he. mix he koi bhi problem nhi he ke. eik elementt me ham Number likh rahe he or eik elementt me ham string likh rahe he
+
+// What is Array?
+// Array eik Object he . ye use hota he eik varieble me multiple collection of items ko store krne ke liye use hota he.
+
+// means mene eik single array diclear kiya " ourHeroes " ab ham us ke andar multiple collection of items ( Multiple Heroes ke naam enter krSkta hoo ) store krskte he
+
+// e.g.
+
+let muslimHeroes = [
+  "Salahuddin Ayyubi",
+  "Malcolm X",
+  "Muhammad Ali",
+  "Ibn Battuta",
+  "Fatima al-Fihri",
+  "Ibn Sina (Avicenna)",
+  "Maryam Mirzakhani",
+  "Al-Farabi",
+  "Hakeem Olajuwon",
+  "Tariq ibn Ziyad",
+  "Nawal El Saadawi",
+  "Mansa Musa",
+  "Rumi",
+  "Shirin Ebadi",
+  "Imam Al-Ghazali",
+  "Khadija bint Khuwaylid",
+  "Al-Razi",
+  "Hamza Yusuf",
+  "Averroes (Ibn Rushd)",
+  "Ibn Khaldun",
+];
+
+// console.log(muslimHeroes);
+
+// Ab mene bola ke multiple Items Add krskte he eik Array me. Question ye he ke agar mujhe 4 number wala call krna hoto me kese kroonga
+
+// Agar Hamein Chahiye ke Hamein 4 number wala call krna hoto iske liye indexing smjhni paregi
+
+// Indexing in Javascript
+
+// JavaScript me indexing ka mtlb hota hai kisi array ya string ke specific element ko access karna. Indexing 0 se start hoti hai, jisse pehla element index 0 par hota hai, doosra index 1 par, aur aise hi aage badhta hai.
+
+// Agar Hamein indexing ka use krke call krwana hoto Array ke naam kesath square bracket laga ke index ka number de denge
+
+// Example
+
+// console.log(muslimHeroes[2]); // Muhammad Ali
+// console.log(muslimHeroes[3]); // Ibn Battuta
+// console.log(muslimHeroes[6]); // Maryam Mirzakhani
+// console.log(muslimHeroes[8]); // Hakeem Olajuwon
+// console.log(muslimHeroes[5]); // Ibn Sina (Avicenna)
+// console.log(muslimHeroes[0]); // Salahuddin Ayyubi
+
+
+
