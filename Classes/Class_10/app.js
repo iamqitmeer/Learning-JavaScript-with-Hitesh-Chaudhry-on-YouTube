c196f7e509c1f24e6142d0f47d37b643e19f6105
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2023-12-27 19:28:36 +0500

    Number and Maths in Javascript

--- Classes/Class_10/app.js
+++ Classes/Class_10/app.js
@@ -17,7 +17,7 @@
 // let myNum = 4000.544322;
 // console.log(myNum.toFixed(2)); // kabhi kabhi ham multiplication value nikaalte waqt hamare precision value bohot bari ho jatii he. obviously client ko itni barii precision value nhii chahiye Hoti. to iske liye ham .toFixed() use krte he. kyu ke ye point ke baad hamare marzi se precision age barhayega. .toFixed(2) // ye 2 baar barhaeiGa
 
-// myNum me Value Bohot Badii He decimal se pehlee bhi OR baad me bhi agar ham chahte he ke apni marzii se value lana to iske Liye ham  .toPrecision() use krte he . isme hamein 1 to 21 tk value de skte he.
+// myNum me Value Bohot Badii He decimal se pehlee bhi OR baad me bhi agar ham chahte he ke apni marzii se value lana to iske Liye ham .toPrecision() use krte he . isme hamein 1 to 21 tk value de skte he.
 
 // console.log(myNum.toPrecision(2)); // 4.0e+3
 
@@ -35,9 +35,12 @@
 
 // console.log(Math); // IsKo bhi ham Number ki Tarah likh skte he. iski bhi Numbe Or string jesi Bohot zyadaa propoerties hoti he
 
-// console.log(Math.abs(-4)); // kayi baar hamein negattive value milti he jo ham nhi chate he ke ye hoo. to ham iskO hataane ke kiye use krte he Math.abs(-4) // ye isko convert krde ga positive me Aisa Nhi he ke negative positive ho jata he positive negative. aisa nhii he sirf negative positive hota he
+// console.log(Math.abs(-4)); // kayi baar hamein negattive value milti he jo ham nhi chate he ke ye hoo. to ham iskohataane ke kiye use krte he Math.abs(-4) // ye isko convert krde ga positive me Aisa Nhi he ke negative positive ho jata he positive negative. aisa nhii he sirf negative positive hota he
 
 // console.log(Math.round(4.3)); // ab kayi baar hamare paas value hoti he decimal me lekin ham chahte he ke ye round off hojaye he matlab decimal ke sat round off ho jaye. to iske liye ham use krte he Math.round(andar Hamein Number denii he)
+
+let myNum = 233.4242
+// console.log(Math.floor(myNum));
 
 // console.log(Math.ceil(4.3)); // agar ham chahte he control krna ke value Upper wali Choose hogi ya Lower wali to ham use krte he .ceil (Mtlab top to Top wala number hi choose kreGA). Matlab decimal ke baad wala number zara sa bhi bara hua to ye decimal se pehle wala number me increament kreGa
 
@@ -47,10 +50,10 @@
 
 // console.log(Math.max(3,4,5,5,6,6,3,5,6,8,7,22,2,3,4,4,5,6,7,8,7,5,4,33,)); // ye badi value nikal dega
 
-console.log(Math.random() * 10); // ye random value deta he. hamesha 0 se 1 ke beech me. agar ham badi value chahte he. to is ke liye ham use krte he  Math.random() ke baad * krdenge ham koi bhi badii value se
+// console.log(Math.random() * 10); // ye random value deta he. hamesha 0 se 1 ke beech me. agar ham badi value chahte he. to is ke liye ham use krte he  Math.random() ke baad * krdenge ham koi bhi badii value se
 
-let min = 10;
-let max = 20;
+// let min = 10;
+// let max = 20;
 
-console.log(Math.floor(Math.random() * (min - max + 1)) + min);
-console.log(Math.floor(Math.random() * (min - max + 1)) + min);
+// console.log(Math.floor(Math.random() * (min - max + 1)) + min);
+// console.log(Math.floor(Math.random() * (min - max + 1)) + min);
